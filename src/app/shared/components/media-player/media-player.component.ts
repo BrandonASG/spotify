41fa18bec78290bb5dbe6bb370720903e938a932
Brandon Sanchez <sagb010916@gmail.com>
2023-02-15 19:30:55 -0600

    22. Modelos (terminado)

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-media-player',
@@ -7,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MediaPlayerComponent implements OnInit {
   
-  mockOver: any = {
+  mockOver: TrackModel = {
     cover: 'https://www.lahiguera.net/musicalia/artistas/bad_bunny/disco/10439/bad_bunny_yhlqmdlg-portada.jpg',
     album: 'YHLQMDLG',
-    name: 'La Santa'
+    name: 'La Santa',
+    url:'http://localhost/track.mp3',
+    _id: '1'
   }
   
   constructor() { }
